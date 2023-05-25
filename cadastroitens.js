f5e87e3ac3ed7e6716f1d81f89469afeebda31a3
Maria Fernanda Vilas Boas <maria.boas3@portalsesisp.org.br>
2023-05-25 17:12:34 -0300

    logica 25/05/2023

---
+++ cadastroitens.js
@@ -0,0 +1,58 @@
+const nomeObra = document.getElementById("nome")
+const descriçao = document.getElementById("idescriçao")
+const fotoa = document.getElementById("foto")
+const botaocadastrar = document.querySelector(".btncadastrar")
+
+botaocadastrar.onclick = (evento) => {
+    evento.preventDefault();
+    fenvio()
+    .then(result =>{
+        if(result){
+            let dados = JSON.parse(localStorage.getItem("catalogo")) || [];
+            dados.push({
+             nome : nome.value,
+             descriçao : descriçao.value,
+             foto: nomeArq,
+
+            })
+            localStorage.setItem("catalogo",JSON.stringify(dados));
+              
+        }
+        else{
+            alert("houve erro no envio do arquivo");
+        }
+    });
+
+  
+}
+
+
+ var nomeArq;
+async function fenvio() {
+    const url = 'http://localhost:3005/upload';
+    const arquivo = document.getElementById("foto").files[0];
+    const formData = new FormData();
+    formData.append('arquivo', arquivo);
+    console.log(JSON.stringify(FormData))
+    try{
+
+        var resp = await fetch(url,{
+                                      method: 'POST',
+                                      body: formData,
+         } )
+         if (resp.ok){
+            let respText = await resp.text();
+            nomeArq = respText;
+            return true;
+         }
+         else{
+            return false;
+         }
+    }
+    catch (error) {
+        console.error(error);
+        return false;
+    }
+   
+}
+
